4fb67cea0ebd19f24d170fdbe976b3dfc5b6a922
David Cortarello <david.cortarello@accenture.com>
2021-01-31 12:50:05 -0300

    Environment or nothing

--- config.py
+++ config.py
@@ -1,12 +1,35 @@
-TUYA_USERNAME = ""
-TUYA_PASSWORD = ""
-TUYA_API_REGION = "us"
-TUYA_COUNTRY_CODE = "54"
-TUYA_BIZ_TYPE = "smart_life" # Could be tuya, smart_life, jinvoo_smart
+import os
+import sys
 
-MIDEA_API_KEY = ""
-MIDEA_USERNAME = ""
-MIDEA_PASSWORD = ""
+try:
+    TUYA_USERNAME = os.environ["TUYA_USERNAME"]
+    TUYA_PASSWORD = os.environ["TUYA_PASSWORD"]
+except:
+    print("TUYA_USERNAME or TUYA_PASSWORD not set")
+    sys.exit(1)
+try:
+    TUYA_API_REGION = os.environ["TUYA_API_REGION"]
+    TUYA_COUNTRY_CODE = os.environ["TUYA_COUNTRY_CODE"]
+    TUYA_BIZ_TYPE = os.environ["TUYA_BIZ_TYPE"]
+except:
+    TUYA_API_REGION = "us"
+    TUYA_COUNTRY_CODE = "54"
+    TUYA_BIZ_TYPE = "smart_life" # Could be tuya, smart_life, jinvoo_smart
 
+try:
+    MIDEA_USERNAME = os.environ["MIDEA_USERNAME"]
+    MIDEA_PASSWORD = os.environ["MIDEA_PASSWORD"]
+except:
+    print("MIDEA_USERNAME or MIDEA_PASSWORD not set")
+    sys.exit(1)
 
-TELEGRAM_BOT_KEY = ""
+try:
+    MIDEA_API_KEY = os.environ["MIDEA_API_KEY"]
+except:
+    MIDEA_API_KEY = '3742e9e5842d4ad59c2db887e12449f9'
+
+try:
+    TELEGRAM_BOT_KEY = os.environ["TELEGRAM_BOT_KEY"]
+except:
+    print("TELEGRAM_BOT_KEY not set")
+    sys.exit(1)
